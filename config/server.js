09838385a552f4192a7d1bcee7858ec270a5dc1e
22FelipeMR03 <feli.rodrigues2007@gmail.com>
2024-06-06 18:31:39 -0300

    06/06/2024

--- config/server.js
+++ config/server.js
@@ -4,6 +4,6 @@ var consign = require("consign");
 var app = express();
 app.set('view engine','ejs'); //informa ao express que serpa usado ejs como engrenagem das views
 app.set('views','./app/views'); // indica o caminho das viwes
-
+app.use(express.static("./app/visual/"));
 consign().include("app/routes").into(app);
 module.exports = app; 
