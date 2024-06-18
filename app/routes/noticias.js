3fcf0331c171a874f7674abcb0d3e3b096be99be
Felipe Machado <feli.rodrigues2007@gmail.com>
2024-06-18 18:22:11 -0300

    18/06/2024

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,9 +1,20 @@
 module.exports = function (app) {
     app.get("/noticias", function (req, res) {
+        console.log("Query= ")
         console.log(req.query);
         var conexao = app.config.dbConnection();
         conexao.query('select * from noticias', function (error, result) {
-            res.render('noticias/noticias', { noticias: result })
+        res.render('noticias/noticias', { noticias: result })
+        
+        });
+    })
+    app.get("/noticias/:id/:id", function (req, res) {
+        console.log("Parametro= ")
+        console.log(req.params);
+        var conexao = app.config.dbConnection();
+        conexao.query('select * from noticias', function (error, result) {
+        res.render('noticias/noticias', { noticias: result })
+        
         });
     })
 }
